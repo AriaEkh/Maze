21d50f06789c6b48e97c0f6fd715f722cf43a8c4
Jacques-Sparrow <fuentes.lucas972@gmail.com>
2017-12-21 18:21:50 +0100

    Separated the maze from Game Class Fixed CreateMaze() and ShowMaze() methods

--- Maze/Maze/Game.cs
+++ Maze/Maze/Game.cs
@@ -9,91 +9,21 @@ namespace Maze
 {
     class Game
     {
-        enum Case {empty, wall, exit};
-        private Case[] Board;
-        private int length;
-        private int lineLength;
+        private Maze maze;
         private int playerCount;
 
 
         public Game(string file)
         {
-            CreateMaze(file);
-        }
-
-        public void CreateMaze(string file)
-        {
-            try
-            {
-                using (StreamReader sr = new StreamReader(file))
-                {                  
-                    String line = sr.ReadLine();
-                    this.lineLength = line.Length;
-                    
-                    sr.BaseStream.Position = 0;
-                    sr.DiscardBufferedData();
-
-                    String fullFile = sr.ReadToEnd();
-                    this.length = fullFile.Length;
-
-                    sr.BaseStream.Position = 0;
-                    sr.DiscardBufferedData();
-
-                    this.Board = new Case[length];
-                    int count = 0;
-                    for (int j = 0; j < length/lineLength; j++)
-                    {
-                        string tempLine = sr.ReadLine();
-                        for (int i = 0; i < tempLine.Length; i++)
-                        {
-                            if (tempLine[i] == '0')
-                            {
-                                Board[count] = Case.empty;
-                                count += 1;
-                            }
-                            else if (tempLine[i] == '1')
-                            {
-                                Board[count] = Case.wall;
-                                count += 1;
-                            }
-                            else if (tempLine[i] == '2')
-                            {
-                                Board[count] = Case.exit;
-                                count += 1;
-                            }
-
-                        }
-                    }
-                    
-                    
-                }
-            }
-            catch(Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            playerCount = 0;
+            maze = new Maze();
+            maze.CreateMaze(file);
         }
 
         public void ShowMaze()
         {
-            int count = 0;
-            for (int j = 0; j < length/lineLength; j++)
-            {
-                for (int i = 0; i < lineLength; i++)
-                {
-                    if (Board[count] == Case.empty || Board[count] == Case.exit)
-                    {
-                        count ++;
-                        Console.Write(" ");
-                    }
-                    else if (Board[count] == Case.wall)
-                    {
-                        count++;
-                        Console.Write("|");
-                    }
-                }
-                Console.WriteLine("\n");
-            }
+            maze.ShowMaze();
         }
-    }
+
+    }       
 }
---
+++ Maze/Maze/Maze.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.IO;
+
+namespace Maze
+{
+    internal class Maze
+    {
+        #region Private Fields
+
+        private Case[] board;
+
+        private int length;
+
+        private double lineLength;
+
+        double lineCount;
+
+        #endregion Private Fields
+
+        #region Private Enums
+
+        private enum Case
+        { empty, wall, exit };
+        #endregion Private Enums
+
+        public Maze()
+        {
+            length = 0;
+            lineLength = 0;
+            lineCount = 0;
+            board = null;
+        }
+
+        #region Public Methods
+
+        public void CreateMaze(string file)
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(file))
+                {
+                    String line = sr.ReadLine();
+                    this.lineLength = line.Length;
+
+                    sr.BaseStream.Position = 0;
+                    sr.DiscardBufferedData();
+
+                    while(sr.EndOfStream==false)
+                    {
+                        sr.ReadLine();
+                        lineCount++;
+                    }
+
+                    sr.BaseStream.Position = 0;
+                    sr.DiscardBufferedData();
+
+                    double temp = lineCount * lineLength;
+                    length = Convert.ToInt32(temp);
+                    this.board = new Case[length];
+                    int count = 0;
+                    for (int j = 0; j < length / lineLength; j++)
+                    {
+                        string tempLine = sr.ReadLine();
+                        for (int i = 0; i < tempLine.Length; i++)
+                        {
+                            if (tempLine[i] == '0')
+                            {
+                                board[count] = Case.empty;
+                                count += 1;
+                            }
+                            else if (tempLine[i] == '1')
+                            {
+                                board[count] = Case.wall;
+                                count += 1;
+                            }
+                            else if (tempLine[i] == '2')
+                            {
+                                board[count] = Case.exit;
+                                count += 1;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public void ShowMaze()
+        {
+            int count = 0;
+            for (int j = 0; j < lineCount; j++)
+            {
+                for (int i = 0; i < lineLength; i++)
+                {
+                    if (board[count] == Case.empty || board[count] == Case.exit)
+                    {
+                        count++;
+                        Console.Write(" ");
+                    }
+                    else if (board[count] == Case.wall)
+                    {
+                        count++;
+                        Console.Write("*");
+                    }
+                }
+                Console.WriteLine("\n");
+            }
+        }
+        #endregion Public Methods
+    }
+}
